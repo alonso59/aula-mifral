91c5d5e47b34d65771a34345959689ca7198c636
alonso <germanpinedo13@gmail.com>
2025-08-03 13:16:27 +0200

    Adapt platform for virtual classroom with teacher/student roles and PDF material management

---
+++ backend/open_webui/roles.py
@@ -0,0 +1,12 @@
+ROLES = {
+    "teacher": {
+        "can_upload": True,
+        "can_instruct": True,
+        "can_view": True,
+    },
+    "student": {
+        "can_upload": False,
+        "can_instruct": False,
+        "can_view": True,
+    }
+}
